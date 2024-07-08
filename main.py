5809407fe92a7de60ce8244dd2cf6be5b89cbcd7
matangi05 <142920658+matangi05@users.noreply.github.com>
2024-07-08 16:03:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-# import speech
-# import intent
-# import commands
-
-# def main():
-#     while True:
-#         print("Listening...")
-#         text = speech.listen_and_recognize()
-#         if text:
-#             intent_name,command_text = intent.determine_intent(text)
-#             if intent_name:
-#                 commands.execute_command(intent_name, command_text)
-#             else:
-#                 print("Sorry, I didn't understand that command.")
-
-# if __name__ == "__main__":
-#     main()
-
 import speech
 import intent
 import commands
@@ -28,7 +10,6 @@ def main():
             intent_name, command_text, input_prompt = intent.determine_intent(text)
             parameters = {}
 
-            # Prompt for parameters if any are needed
             if input_prompt:
                 input_keys = [key.strip("{}") for key in command_text.split() if "{" in key and "}" in key]
                 for key in input_keys:
