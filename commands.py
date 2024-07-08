d4d94f15ece00f15b927810fc18f2f5d28b95416
matangi05 <142920658+matangi05@users.noreply.github.com>
2024-07-08 16:04:05 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,35 +1,3 @@
-# import subprocess
-# import os
-# import platform
-
-# def execute_command(intent_name, command_text=None):
-#     if intent_name == "greeting":
-#         print("Hello! How can I help you?")
-#     elif intent_name == "goodbye":
-#         print("Goodbye!")
-#         exit()
-#     elif intent_name == "no":
-#         print("no")
-#         exit()
-
-#     elif intent_name == "open_file":
-#         file_path="requirements.txt"
-#         os.system(r'requirements.txt')
-        
-#     elif intent_name == "create_react_app" and command_text:
-#         app_name = command_text.strip()
-#         if app_name:
-#             try:
-#                 os.system(f'npm create vite@latest {app_name}')
-#                 print(f"React app '{app_name}' created successfully.")
-#             except Exception as e:
-#                 print(f"An error occurred while creating React app: {e}")
-#         else:
-#             print("No app name provided.")
-#     else:
-#         print("Command not recognized.")
-
-
 import os
 
 def execute_command(intent_name, command_text=None, parameters={}):
